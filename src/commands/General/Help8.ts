2adf150c7931c4ae8d188a6c0434a9a198c56b00
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-03 17:38:01 +0530

    Update Help8.ts

--- src/commands/General/Help8.ts
+++ src/commands/General/Help8.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            './assets/Rin/rin.mp4','./assets/Rin/rin-1.mp4','./assets/Rin/rin-2.mp4','./assets/Rin/rin-3.mp4','./assets/Rin/rin-4.mp4','./assets/Rin/rin-5.mp4','./assets/Rin/rin-6.mp4'
+            './assets/Rin/rin.mp4','./assets/Rin/marin edit _ woman(720P_HD).mp4'
         ]
         let well = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: well }, MessageType.video, {quoted:M.WAMessage,
