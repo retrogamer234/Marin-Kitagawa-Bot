3d28e5f9ca374d9477fbc5f2a9178d7b2ac814f6
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-04 04:24:49 +0530

    Update Do-you-mean.ts

--- src/commands/General/Do-you-mean.ts
+++ src/commands/General/Do-you-mean.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://i.ibb.co/mtYJqk0/Marinhot.gif'
+            'https://telegra.ph/file/ab6e446ef62988830e8be.mp4'
         ]
         let chitoge = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
